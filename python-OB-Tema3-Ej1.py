e6eeb0de9ff6c995e2a40e83133722e4a1de1f7d
unknown <ana.vazquez.s@gmail.com>
2022-06-09 01:43:32 -0300

    primer commit Ana

---
+++ python-OB-Tema3-Ej1.py
@@ -0,0 +1,19 @@
+from operator import truediv
+
+
+nombre = 'Luisina'
+apellido = 'Unapellido'
+edad = 28
+email = 'l-unapellido@mimail.com'
+telefono = '125-55489'
+casado = True
+con_hijos = False
+lista_de_amigos = ('Mariela', 'Roberto', 'Jeronimo', 'Andrea')
+peliculas_vistas = {
+    "titulo1": "Corazon valiente",
+    "titulo2": "El hijo de la novia",
+    "titulo3": "No mires arriba"
+}
+print("Nombre y apellido: ", nombre, " ", apellido, "; edad: ", edad, "; email: ", email, "; telefono: ", telefono, "; casado/a?: ", casado, "; con hijos?: ", con_hijos)
+print("Lista de amigos: ", lista_de_amigos)
+print("Peliculas vistas: ", peliculas_vistas)
